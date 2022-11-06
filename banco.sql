69622e585fec1774dc5f1d29aa63078881859102
Ernani Gomes <lic.fisica.cederj@gmail.com>
2022-11-06 19:51:53 -0300

    Ajustar sql do banco

--- banco.sql
+++ banco.sql
@@ -1,7 +1,7 @@
 CREATE TABLE dados (
-    produtoID int,
-    codigo_produto varchar(50),
-		nome_produto varchar(50),
-    descricao varchar(150),
-    fabricante varchar(150)
+	produtoID int,
+	codigo_produto varchar(50),
+	nome_produto varchar(50),
+	descricao varchar(150),
+	fabricante varchar(150)
 );
